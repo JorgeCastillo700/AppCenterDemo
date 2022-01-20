a07304383ef9ae0b648543120e7cf15496579a7e
JCastillo <jorge.castillo.700@gmail.com>
2022-01-20 09:38:45 -0800

    add analytics
    track events with analytics

--- RetirementCalculator/RetirementCalculator/ViewController.swift
+++ RetirementCalculator/RetirementCalculator/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import AppCenterCrashes
+import AppCenterAnalytics
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var uiButton: UIButton!
+    
+    @IBOutlet weak var lastNameTextView: UITextField!
+    @IBOutlet weak var firstNameTextView: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func didTouch(_ sender: Any) {
+        let firstName: String? = firstNameTextView.text
+        let lastName: String? = lastNameTextView.text
+        
+        let properties = ["firstName": firstName!, "lastName": lastName!]
+        
+        Analytics.trackEvent("button_was_pressed", withProperties: properties)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        Analytics.trackEvent("load_main_view")
+    }
+    
 }
 
